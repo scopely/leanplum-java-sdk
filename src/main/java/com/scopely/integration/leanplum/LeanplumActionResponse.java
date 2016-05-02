8f28ae42ddaee8cc9a6c9d5b2c2e4ab32698ab18
Cristian <me@cristian.io>
2016-05-02 16:46:07 -0700

    Add Exportuser action

--- src/main/java/com/scopely/integration/leanplum/LeanplumActionResponse.java
+++ src/main/java/com/scopely/integration/leanplum/LeanplumActionResponse.java
@@ -1,10 +1,13 @@
 package com.scopely.integration.leanplum;
 
+import java.util.Map;
+
 public class LeanplumActionResponse {
     public boolean success;
     public boolean isOffline;
     public LeanplumError error;
     public LeanplumWarning warning;
+    public Map<String, Object> userAttributes;
 
     @Override
     public String toString() {
@@ -12,7 +15,8 @@ public class LeanplumActionResponse {
                 "success=" + success +
                 ", isOffline=" + isOffline +
                 ", error=" + error +
-                ", message=" + warning +
+                ", warning=" + warning +
+                ", userAttributes=" + userAttributes +
                 '}';
     }
 }
